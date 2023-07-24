508be37dd235dd24b33b8b964a2ec16f62a2b979
Q <q7path@gmail.com>
2023-07-24 20:23:13 +0300

    remove font

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,9 +6,6 @@
 
         <title>My code 2</title>
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
 
         <!-- Styles -->
         <style>
