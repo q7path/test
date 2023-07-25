d14be97883957eeda1c260456530bad14a2d19aa
Q <q7path@gmail.com>
2023-07-25 18:03:10 +0300

    add h2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -26,8 +26,10 @@
     </head>
     <body class="antialiased">
         <div class="firsted">
-            Let's work
-            <small class="wow"><sup>new</sup></small>
+            <h2>
+                Let's work
+                <small class="wow"><sup>new</sup></small>
+            </h2>
         </div>
         <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
             @if (Route::has('login'))
